348b2ea3448254c29d2d19cf37a9990722f360cb
Sateesh1042 <gorakalasateesh1042@gmail.com>
2024-05-29 23:43:51 +0530

    updated Version

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,7 +1,9 @@
 import CartList from "./components/CartList";
 import CartEmpty from"./components/CartEmpty";
-import { useCart } from "../../context";
+
 import useTitle from "../../hooks/useTitle";
+import { useCart } from "../../context";
+
 const CartPage = () => {
   useTitle("Cart");
   const {cartList} =useCart();
